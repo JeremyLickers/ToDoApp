d9fe53de9971ebbd3a541aec3bdd8fef8ecdb2b1
Jeremy Lickers <jeremylickers.1996@gmail.com>
2023-08-07 17:04:49 -0400

    added menu, task form, and task list

--- to_do_app/src/App.js
+++ to_do_app/src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
-import Task from "./Task";
+import Menu from "./menu";
 function App() {
-  const title = "Hello World";
-  const likes = 50;
-
   return (
     <div className="App">
-      <div className="content">
-        <h1> {title} </h1>
-        <p>Liked {likes} times</p>
-      </div>
-      <Task />
+      <div className="content"></div>
+      <Menu />
     </div>
   );
 }
--- to_do_app/src/Task.js
+++ to_do_app/src/Task.js
@@ -1,24 +1,8 @@
-import { useRef, useState } from "react";
-
-const Task = () => {
-  const ref = useRef(null);
-  const handleClick = () => {
-    ref.current.value = "";
-  };
-
-  return (
-    <div>
-      <h1>your text here</h1>
-      <textarea ref={ref}>Your Text Here</textarea>
-      <button
-        onClick={() => {
-          handleClick();
-        }}
-      >
-        Delete Text
-      </button>
-    </div>
-  );
-};
-
-export default Task;
+const Task = (props) => {
+    return (
+        {name: props.name,
+        startDate: props.startDate,
+        endDate: props.endDate,
+        descritipn: props.descritipn}
+    )
+}
