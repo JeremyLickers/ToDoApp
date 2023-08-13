f1eab1e886f67f5f95ac7652d987a3f205ef62b4
Jeremy Lickers <jeremylickers.1996@gmail.com>
2023-08-13 17:31:22 -0400

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import Menu from "./menu";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom";
+import TaskForm from "./task-form";
 function App() {
   return (
-    <div className="App">
-      <div className="content"></div>
-      <Menu />
-    </div>
+    <Router>
+      <div className="App">
+        <div className="content"></div>
+        <Switch>
+          <Route exact path="/">
+            <Menu />
+          </Route>
+          <Route path="/task-form">
+            <TaskForm/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/menu.js
+++ src/menu.js
@@ -1,24 +1,34 @@
 import { useState, useEffect } from 'react';
-import TaskForm from "./task-form";
+import { Link } from 'react-router-dom';
 import TaskList from "./task-list";
 
 const Menu = () => {
     const [tasks, setTasks] = useState(null);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/tasks')
-            .then(response => {
-                return response.json()
-                .then(data => {
-                    setTasks(data)
-                })
+            .then(res => {
+                if(!res.ok){
+                    throw Error('Database connection error');
+                }
+                return res.json();
             })
-    }, [])
-
+            .then(data => {
+                setTasks(data);
+                setError(null);
+            })
+            .catch(err => {
+                    console.log('err--',err)
+                    setError(err.message);
+            })
+    }, []);
+    console.log(error)
     return (
         <div>
-            {tasks && <TaskList tasks={tasks}/> }
-            <TaskForm />
+            { error && <div> {error} </div> }
+            { tasks && <TaskList tasks={tasks}/> }
+            <Link to="/task-form">Create New Task</Link>
         </div>
   );
 };
